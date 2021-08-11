edeb057086b118034816e26a274188958426430e
david <realdamage666@gmail.com>
2021-08-11 16:56:53 +0600

    Add react-redux

--- src/components/chatContainer.jsx
+++ src/components/chatContainer.jsx
@@ -2,29 +2,50 @@ import '../styles/chat.css'
 import React from 'react';
 import { addMessageActCreator, updateNewMessageTextActCreator } from '../redux/dialog-reducer'
 import Chat from './chat';
-import storeContext from '../storeContext';
-const ChatContainer = () =>{
+import { connect } from 'react-redux'
+// const ChatContainer = () =>{
     
-    return(
-    <storeContext.Consumer>{
-        (store) =>{
-            let state = store.getState() 
+//     return(
+//     <storeContext.Consumer>{
+//         (store) =>{
+//             let state = store.getState() 
 
 
-            let GetMessages = () =>{
-                store.dispatch(addMessageActCreator())
+//             let GetMessages = () =>{
+//                 store.dispatch(addMessageActCreator())
                 
-            }
-            let getNewMessageText = (msg) =>{
-                store.dispatch(updateNewMessageTextActCreator(msg))
+//             }
+//             let getNewMessageText = (msg) =>{
+//                 store.dispatch(updateNewMessageTextActCreator(msg))
                
-            }
-            return  <Chat sendMessage={GetMessages} updateNewMessageText={getNewMessageText} messages={state.dialogPage.messages} NewMessageText={state.dialogPage.newMessageText} name={state.dialogPage.dialogData[0].name} />
-        }    
-    }
-    </storeContext.Consumer>
-    );
+//             }
+//             return  <Chat sendMessage={GetMessages} updateNewMessageText={updateNewMessageText} messages={state.dialogPage.messages} NewMessageText={state.dialogPage.newMessageText} name={state.dialogPage.dialogData[0].name} />
+//         }    
+//     }
+//     </storeContext.Consumer>
+//     );
     
    
+// }
+
+let mapStateToProps = (state) =>{
+    return {
+        messages : state.dialogPage.messages,
+        NewMessageText : state.dialogPage.newMessageText,
+        name : state.dialogPage.dialogData[0].name
+    }
 }
+let mapDispatchToProps = (dispatch) => {
+    return {
+        sendMessage : () =>{
+            dispatch(addMessageActCreator())
+        },
+        updateNewMessageText : (msg) => {
+            dispatch(updateNewMessageTextActCreator(msg))
+        }
+    }
+}
+const ChatContainer = connect(mapStateToProps, mapDispatchToProps) (Chat)
+
+
 export default ChatContainer
--- src/components/newsContainer.jsx
+++ src/components/newsContainer.jsx
@@ -1,30 +1,47 @@
 import React from 'react'
 import { addPostActCreator, updateNewPostTextActCreator } from '../redux/news-reducer'
-import storeContext from '../storeContext'
 import '../styles/news.css'
 import News from './news'
-const NewsContainer = () => {
+import { connect } from 'react-redux'
+// const NewsContainer = () => {
     
 
 
-    return (
-        <storeContext.Consumer>{
-            (store) => {
-                let state = store.getState()
+//     return (
+//         <storeContext.Consumer>{
+//             (store) => {
+//                 let state = store.getState()
     
 
-                let post = () => {
-                    store.dispatch(addPostActCreator())
+//                 let post = () => {
+//                     store.dispatch(addPostActCreator())
                     
-                }
-                let getNewText = (text) => {
-                   store.dispatch(updateNewPostTextActCreator(text))
+//                 }
+//                 let getNewText = (text) => {
+//                    store.dispatch(updateNewPostTextActCreator(text))
                    
-                }
-                return <News addPost={post} updateNewPostText={getNewText} posts={state.newsPage.posts} newPostText={state.newsPage.newPostText}/> 
-            }   
-        }     
-        </storeContext.Consumer>
-    );
-}
+//                 }
+//                 return <News addPost={post} updateNewPostText={getNewText} posts={state.newsPage.posts} newPostText={state.newsPage.newPostText}/> 
+//             }   
+//         }     
+//         </storeContext.Consumer>
+//     );
+    let mapStateToProps = (state) =>{
+        return {
+            posts : state.newsPage.posts,
+            newPostText : state.newsPage.newPostText
+        }
+    }
+    let mapDispatchToProps = (dispatch) =>{
+        return {
+            addPost : () => {
+                dispatch(post)
+            },
+            updateNewPostText : (text) => {
+                dispatch(getNewText(text))
+            }
+        }
+    }
+    const NewsContainer = connect (mapStateToProps,mapDispatchToProps) (News);
+
 export default NewsContainer
